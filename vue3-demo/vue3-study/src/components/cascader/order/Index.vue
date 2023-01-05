8b93a2f0c25ef8f0287bb6fce49b87ded9878b84
yuri-li <javaliyuanyuan@163.com>
2023-01-05 11:21:41 +0800

    feat:自定义级联数据格式

---
+++ vue3-demo/vue3-study/src/components/cascader/order/Index.vue
@@ -0,0 +1,28 @@
+<template>
+  <ElCascader v-model="model" :options="earlyCourse" :props="props" style="width:220px;" @change="handleChange" />
+</template>
+
+<script setup lang="ts">
+import { defineOptions } from "unplugin-vue-define-options/macros"
+import { ElCascader } from "element-plus"
+import { earlyCourse } from "@/components/cascader/order/model"
+import { ref } from "vue"
+
+defineOptions({
+  name: "CustomCascaderData",
+})
+
+const model = ref([])
+const props = {
+  expandTrigger: "hover",
+}
+
+function handleChange() {
+  console.log(model.value.length)
+  console.log(model.value)
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- vue3-demo/vue3-study/src/config/routes.ts
+++ vue3-demo/vue3-study/src/config/routes.ts
@@ -1,17 +1,34 @@
-import { createRouter, createWebHashHistory } from "vue-router"
+import { createRouter, createWebHashHistory, NavigationGuardNext, RouteLocationNormalized } from "vue-router"
 
-const routes = [{path: "/", redirect: {name: "CascaderAddress"}},
-    {path: "/helloWorld", name: "HelloWorld", component: () => import("@/components/HelloWorld.vue")},
-    {path: "/reverseString", name: "ReverseString", component: () => import("@/components/ReverseString.vue")},
-    { path: "/login", name: "Login", component: () => import("@/components/login/Index.vue") },
-    { path: "/form", name: "Form", component: () => import("@/components/form/Index.vue") },
-    { path: "/cascader/address", name: "CascaderAddress", component: () => import("@/components/cascader/address/Index.vue") },
-    { path: "/multiplicationTable", name: "MultiplicationTable", component: () => import("@/components/multiplicationTable/Index.vue")},
-    {path: "/multiplicationTable/item", name: "MultiplicationTableItem", component: () => import("@/components/multiplicationTable/Item.vue")}]
+const routes = [{path: "/", redirect: {name: "CustomCascaderData"}}, {
+    path: "/helloWorld", name: "HelloWorld", component: () => import("@/components/HelloWorld.vue")
+}, {path: "/reverseString", name: "ReverseString", component: () => import("@/components/ReverseString.vue")}, {
+    path: "/login", name: "Login", component: () => import("@/components/login/Index.vue"), meta: {title: "登录页面"},
+}, {path: "/form", name: "Form", component: () => import("@/components/form/Index.vue"), meta: {title: "Form表单"},}, {
+    path: "/cascader/address", name: "CascaderAddress", component: () => import("@/components/cascader/address/Index.vue"), meta: {title: "三级地址级联"},
+}, {
+    path: "/cascader/order", name: "CustomCascaderData", component: () => import("@/components/cascader/order/Index.vue"), meta: {title: "自定义级联数据格式"},
+}, {
+    path: "/multiplicationTable", name: "MultiplicationTable", component: () => import("@/components/multiplicationTable/Index.vue")
+}, {path: "/multiplicationTable/item", name: "MultiplicationTableItem", component: () => import("@/components/multiplicationTable/Item.vue")}]
 
 
 const router = createRouter({
     history: createWebHashHistory(), routes,
 })
+
+router.beforeEach((to: RouteLocationNormalized, from: RouteLocationNormalized, next: NavigationGuardNext) => {
+    buildTitle(to.meta.title as string, to.params.pageTitle)
+    next()
+})
+
+function buildTitle(title?: string, titleFromParams?: string | string[]) {
+    if (title) {
+        document.title = title
+    }
+    if (titleFromParams) {
+        document.title = `${titleFromParams} - ${document.title}`
+    }
+}
 
 export { router }
